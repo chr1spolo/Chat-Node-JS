c9e7e5729c033687f334af561835b021f9310862
chr1spolo <polidavis28@gmail.com>
2017-03-12 18:51:48 -0500

    Simple chat usando sockets.io

--- public/main.js
+++ public/main.js
@@ -2,4 +2,28 @@ var socket = io.connect('http://localhost:8000', { 'forceNew' : true});
 
 socket.on('messages', function(data) {
   console.log(data);
+  render(data);
 });
+
+function render(data) {
+
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <b>${elem.author}</b> <br>
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" <br> ");
+
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(element) {
+  var payload = {
+    author : document.getElementById('username').value,
+    text   : document.getElementById('texto').value,
+  };
+
+  socket.emit('new-messages', payload);
+  return false;
+}
--- server/index.js
+++ server/index.js
@@ -2,21 +2,22 @@ var express = require('express');
 var app     = express();
 var server  = require('http').Server(app);
 var io      = require('socket.io')(server);
+var messages = [{
+  id     : 1,
+  text   : "Primer mensaje",
+  author : "Christian Polo"
+}];
 
 app.use(express.static('public'));
 
 
-app.get('/', function(req, res) {
-  res.status(200).send('Hola mundo');
-});
-
-
 io.on('connection', function(socket) {
-  console.log('Alguien se ha conectado con Sockets');
-  socket.emit('messages', {
-    "id"     : 1,
-    "text"   : "Hola soy un mensaje",
-    "author" : "Cristian Polo"
+
+  socket.emit('messages', messages);
+
+  socket.on('new-messages', function(data) {
+    messages.push(data);
+    io.sockets.emit('messages', messages);
   });
 });
 
